7f41b12e5267d36392f9ec3572a1a9d7fbc84db6
Aryan <bhattiaryan777@gmail.com>
2026-07-30 08:39:03 +0530

    theme changing and main page layout done

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const username_header = document.querySelector(".name")
 const user = JSON.parse(localStorage.getItem("user"))
 
+// set username in header
+
 username_header.innerHTML = user.username
+
+
+ const darkModeToggle = document.getElementById('darkModeToggle');
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-theme');
+        darkModeToggle.checked = true;
+    }
+    darkModeToggle.addEventListener('change', (e) => {
+        if (e.target.checked) {
+            document.body.classList.add('dark-theme');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.classList.remove('dark-theme');
+            localStorage.setItem('theme', 'light');
+        }
+    });
