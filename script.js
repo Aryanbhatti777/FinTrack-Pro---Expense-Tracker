016746636b557aebcb441aa7bddbd831397d4a03
Aryan <bhattiaryan777@gmail.com>
2026-07-30 12:49:55 +0530

    Add transaction done

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const user = JSON.parse(localStorage.getItem("user"))
 const addTransactionBtn = document.querySelector("#openAddModalBtn")
 const txForm = document.querySelector(".add_transaction")
 const closeForm = document.querySelector(".close-modal")
+const form = document.querySelector("form");
+const transactionStorageKey = `transactions_${user.username}`
+
 
 // make transaction form visible and hidden
 
@@ -34,3 +37,39 @@ username_header.innerHTML = user.username
             localStorage.setItem('theme', 'light');
         }
     });
+
+
+//add transaction
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const tx_type = form.type.value
+    const description = form.description.value;
+    const amount = form.amount.value 
+    const tx_date = form.Tdate.value 
+    const category = form.category.value;
+    
+    if(description.trim() === "" || amount.trim() === "" ||  category.trim() === ""){
+        alert("All fields are mandatory")
+    }
+
+    const newTransaction = {
+        id: Date.now(),
+        transactionType : tx_type,
+        description,
+        amount,
+        transactionDate : tx_date,
+        category
+    }
+    
+    const transactions = JSON.parse(localStorage.getItem(transactionStorageKey)) || []
+
+    transactions.push(newTransaction)
+
+    localStorage.setItem(transactionStorageKey, JSON.stringify(transactions))
+
+    console.log(transactions)
+
+})
