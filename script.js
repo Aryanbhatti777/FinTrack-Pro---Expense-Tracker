adb36e2a10a9da7bd019192b3ccc8efd6c5d2cce
Aryan <bhattiaryan777@gmail.com>
2026-07-30 09:24:08 +0530

    transaction form done

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const username_header = document.querySelector(".name")
 const user = JSON.parse(localStorage.getItem("user"))
+const addTransactionBtn = document.querySelector("#openAddModalBtn")
+const txForm = document.querySelector(".add_transaction")
+const closeForm = document.querySelector(".close-modal")
+
+// make transaction form visible and hidden
+
+addTransactionBtn.addEventListener("click", () => {
+    txForm.style.display = "flex"
+})
+
+closeForm.addEventListener("click", () => {
+    txForm.style.display = "none"
+})
 
 // set username in header
 
 username_header.innerHTML = user.username
 
+// theme changing
 
  const darkModeToggle = document.getElementById('darkModeToggle');
     if (localStorage.getItem('theme') === 'dark') {
